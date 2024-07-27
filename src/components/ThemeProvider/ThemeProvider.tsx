2e36db174fe89477605878ccb5f126e4ef66b21e
Vanshika Sabharwal <vanshikasabharwal06@gmail.com>
2024-07-27 19:33:13 +0530

    added type of modules

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,42 +1,36 @@
 "use client";
 
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import ThemeContext from "@/context/themeContext";
 
 type ThemeProviderProps = {
   children: React.ReactNode;
 };
 
-const ThemeProvider: React.FC<ThemeProviderProps> = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
-  const themeLocalStorage: boolean =
+const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const initialTheme =
     typeof localStorage !== "undefined" && localStorage.getItem("hotel-theme")
-      ? JSON.parse(localStorage.getItem("hotel-theme")!)
+      ? JSON.parse(localStorage.getItem("hotel-theme") as string)
       : false;
 
-  const [darkTheme, setDarkTheme] = useState<boolean>(themeLocalStorage);
+  const [darkTheme, setDarkTheme] = useState<boolean>(initialTheme);
 
-  const [renderComponent, setRenderComponent] = useState(false);
   useEffect(() => {
-    setRenderComponent(true);
-  }, []);
-
-  if (!renderComponent) return <></>;
+    try {
+      localStorage.setItem("hotel-theme", JSON.stringify(darkTheme));
+    } catch (error) {
+      console.error("Error storing theme in localStorage:", error);
+    }
+  }, [darkTheme]);
 
   return (
-    <div className="theme-provider">
-      <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
-        <div className={`${darkTheme ? "dark" : ""} min-h-screen`}>
-          <div className="dark:text-white dark:bg-black text-[##1E1E1E]">
-            {children}
-          </div>
+    <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+      <div className={`${darkTheme ? "dark" : ""} min-h-screen`}>
+        <div className="dark:text-white dark:bg-black text-[##1E1E1E]">
+          {children}
         </div>
-      </ThemeContext.Provider>
-    </div>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
